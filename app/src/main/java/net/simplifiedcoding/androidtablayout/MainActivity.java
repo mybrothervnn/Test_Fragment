6c374ef09c29409c525e015e5a5c5927628da96a
Thanh 2 <mybrothervnn@gmail.com>
2018-01-18 09:40:38 +0700

    1.create branch test Fragment

--- app/src/main/java/net/simplifiedcoding/androidtablayout/MainActivity.java
+++ app/src/main/java/net/simplifiedcoding/androidtablayout/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 //Implementing the interface OnTabSelectedListener to our MainActivity
 //This interface would help in swiping views
@@ -45,6 +48,13 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
 
         //Adding onTabSelectedListener to swipe views
         tabLayout.setOnTabSelectedListener(this);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        test();
+    }
+
+    private void test() {
+        //xóa hiển thị của 1 tab
+
     }
 
     @Override
@@ -60,5 +70,34 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
     @Override
     public void onTabReselected(TabLayout.Tab tab) {
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Khởi tạo notification_menu cho menu khi CreateOptionMenu
+        getMenuInflater().inflate(R.menu.notification_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.actionbar_item0:
+                tabLayout.removeTab(tabLayout.getTabAt(1));
+                break;
+            case R.id.actionbar_item1:
+                break;
+            case R.id.actionbar_item:
+                break;
+            case R.id.actionbar_item2:
+                break;
+            case R.id.actionbar_item3:
+                break;
+            case R.id.actionbar_item4:
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/net/simplifiedcoding/androidtablayout/Pager.java
+++ app/src/main/java/net/simplifiedcoding/androidtablayout/Pager.java
@@ -24,19 +24,7 @@ public class Pager extends FragmentStatePagerAdapter {
     @Override
     public Fragment getItem(int position) {
         //Returning the current tabs
-        switch (position) {
-            case 0:
-                Tab1 tab1 = new Tab1();
-                return tab1;
-            case 1:
-                Tab2 tab2 = new Tab2();
-                return tab2;
-            case 2:
-                Tab3 tab3 = new Tab3();
-                return tab3;
-            default:
-                return null;
-        }
+        return Tab.newInstance(position);
     }
 
     //Overriden method getCount to get the number of tabs
